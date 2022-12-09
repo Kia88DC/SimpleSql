5c8b51070a301c2ae4e47485e32e845f0e2cc2e0
Kiarash shahbazi <77072333+Kia88DC@users.noreply.github.com>
2022-12-09 16:43:58 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name = 'SimpleSql',
+    version = '2.0.0',
+    description= 'A USER-FRIENDLY package built for working with <sqlite>',
+    author = 'Kiarash Shahbazi',
+    packages=['SimpleSql']
+)
