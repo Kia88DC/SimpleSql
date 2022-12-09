049aa45146591556c06090a80f41b704469ee8dd
Kiarash shahbazi <77072333+Kia88DC@users.noreply.github.com>
2022-12-09 16:49:12 +0330

    Version 3.0.0

---
+++ SimpleSql-3.0.0/SimpleSql/__init__.py
@@ -0,0 +1,11 @@
+#  -----------------
+# |                 |
+# |   By @Kia88DC   |
+# |                 |
+#  -----------------
+# -Refrence 
+# -- github:
+# ---- https://github.com/Kia88DC/SimpleSql
+__version__ = "3.0.0"
+
+from .SimpleSql import sql
---
+++ SimpleSql-3.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name = 'SimpleSql',
+    version = '3.0.0',
+    description= 'A USER-FRIENDLY package built for working with <sqlite>',
+    author = 'Kiarash Shahbazi',
+    packages=['SimpleSql']
+)
